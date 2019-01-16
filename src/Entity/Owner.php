dbead00d9bb5b48eb670f446d2e2a6f4d2787bcc
Nicopuissance <nicolasduwavrent@hotmail.fr>
2019-01-16 10:56:57 +0100

    Add relation to Owner Entity

--- src/Entity/Owner.php
+++ src/Entity/Owner.php
@@ -2,6 +2,8 @@
 
 namespace App\Entity;
 
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
 /**
@@ -22,6 +24,16 @@ class Owner
      */
     private $user_id;
 
+    /**
+     * @ORM\OneToMany(targetEntity="App\Entity\Housing", mappedBy="owner_id", orphanRemoval=true)
+     */
+    private $housings;
+
+    public function __construct()
+    {
+        $this->housings = new ArrayCollection();
+    }
+
     public function getId(): ?int
     {
         return $this->id;
@@ -35,6 +47,37 @@ class Owner
     public function setUserId(User $user_id): self
     {
         $this->user_id = $user_id;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection|Housing[]
+     */
+    public function getHousings(): Collection
+    {
+        return $this->housings;
+    }
+
+    public function addHousing(Housing $housing): self
+    {
+        if (!$this->housings->contains($housing)) {
+            $this->housings[] = $housing;
+            $housing->setOwnerId($this);
+        }
+
+        return $this;
+    }
+
+    public function removeHousing(Housing $housing): self
+    {
+        if ($this->housings->contains($housing)) {
+            $this->housings->removeElement($housing);
+            // set the owning side to null (unless already changed)
+            if ($housing->getOwnerId() === $this) {
+                $housing->setOwnerId(null);
+            }
+        }
 
         return $this;
     }
