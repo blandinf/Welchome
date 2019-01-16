3db8f83eadf0da232d0e10225a2084bdf6f56e06
Nicopuissance <nicolasduwavrent@hotmail.fr>
2019-01-16 10:39:25 +0100

    Owner entity created

---
+++ src/Entity/Owner.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\OwnerRepository")
+ */
+class Owner
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\OneToOne(targetEntity="App\Entity\User", cascade={"persist", "remove"})
+     * @ORM\JoinColumn(nullable=false)
+     */
+    private $user_id;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getUserId(): ?User
+    {
+        return $this->user_id;
+    }
+
+    public function setUserId(User $user_id): self
+    {
+        $this->user_id = $user_id;
+
+        return $this;
+    }
+}
