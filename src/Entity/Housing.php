89ca48d3cdd150a693507314b6fd91034f7baa2b
Nicopuissance <nicolasduwavrent@hotmail.fr>
2019-01-16 11:05:01 +0100

    Housing entity update

--- src/Entity/Housing.php
+++ src/Entity/Housing.php
@@ -2,6 +2,8 @@
 
 namespace App\Entity;
 
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
 /**
@@ -72,6 +74,16 @@ class Housing
      * @ORM\Column(type="integer")
      */
     private $area;
+
+    /**
+     * @ORM\OneToMany(targetEntity="App\Entity\Photo", mappedBy="housing_id", orphanRemoval=true)
+     */
+    private $photos;
+
+    public function __construct()
+    {
+        $this->photos = new ArrayCollection();
+    }
 
     public function getId(): ?int
     {
@@ -206,6 +218,37 @@ class Housing
     public function setArea(int $area): self
     {
         $this->area = $area;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection|Photo[]
+     */
+    public function getPhotos(): Collection
+    {
+        return $this->photos;
+    }
+
+    public function addPhoto(Photo $photo): self
+    {
+        if (!$this->photos->contains($photo)) {
+            $this->photos[] = $photo;
+            $photo->setHousingId($this);
+        }
+
+        return $this;
+    }
+
+    public function removePhoto(Photo $photo): self
+    {
+        if ($this->photos->contains($photo)) {
+            $this->photos->removeElement($photo);
+            // set the owning side to null (unless already changed)
+            if ($photo->getHousingId() === $this) {
+                $photo->setHousingId(null);
+            }
+        }
 
         return $this;
     }
