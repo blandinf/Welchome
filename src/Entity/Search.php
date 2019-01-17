8c246daf8a9c4c59e8244d2f24a3582e53b25e66
Nicopuissance <nicolasduwavrent@hotmail.fr>
2019-01-17 14:27:59 +0100

    Search entity created

---
+++ src/Entity/Search.php
@@ -0,0 +1,125 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\SearchRepository")
+ */
+class Search
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $location;
+
+    /**
+     * @ORM\Column(type="integer", nullable=true)
+     */
+    private $adults;
+
+    /**
+     * @ORM\Column(type="integer", nullable=true)
+     */
+    private $children;
+
+    /**
+     * @ORM\Column(type="integer", nullable=true)
+     */
+    private $babies;
+
+    /**
+     * @ORM\Column(type="datetime", nullable=true)
+     */
+    private $startDate;
+
+    /**
+     * @ORM\Column(type="datetime", nullable=true)
+     */
+    private $endDate;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getLocation(): ?string
+    {
+        return $this->location;
+    }
+
+    public function setLocation(string $location): self
+    {
+        $this->location = $location;
+
+        return $this;
+    }
+
+    public function getAdults(): ?int
+    {
+        return $this->adults;
+    }
+
+    public function setAdults(?int $adults): self
+    {
+        $this->adults = $adults;
+
+        return $this;
+    }
+
+    public function getChildren(): ?int
+    {
+        return $this->children;
+    }
+
+    public function setChildren(?int $children): self
+    {
+        $this->children = $children;
+
+        return $this;
+    }
+
+    public function getBabies(): ?int
+    {
+        return $this->babies;
+    }
+
+    public function setBabies(?int $babies): self
+    {
+        $this->babies = $babies;
+
+        return $this;
+    }
+
+    public function getStartDate(): ?\DateTimeInterface
+    {
+        return $this->startDate;
+    }
+
+    public function setStartDate(?\DateTimeInterface $startDate): self
+    {
+        $this->startDate = $startDate;
+
+        return $this;
+    }
+
+    public function getEndDate(): ?\DateTimeInterface
+    {
+        return $this->endDate;
+    }
+
+    public function setEndDate(?\DateTimeInterface $endDate): self
+    {
+        $this->endDate = $endDate;
+
+        return $this;
+    }
+}
